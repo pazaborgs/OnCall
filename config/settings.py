d3ab2c38e780b976bff069e6951756fd2ea8317f
pazaborgs <patsrgsreis@gmail.com>
2026-01-29 11:45:26 -0300

    Fix: Configurando STORAGES para WhiteNoise

--- config/settings.py
+++ config/settings.py
@@ -123,7 +123,15 @@ TEMPLATES = [
 STATIC_URL = "static/"
 STATICFILES_DIRS = [os.path.join(BASE_DIR, "static")]
 STATIC_ROOT = BASE_DIR / "staticfiles"
-# STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
+STORAGES = {
+    "default": {
+        "BACKEND": "django.core.files.storage.FileSystemStorage",
+    },
+    "staticfiles": {
+        "BACKEND": "whitenoise.storage.CompressedStaticFilesStorage",
+    },
+}
+
 # MEDIA_URL = "media/"
 # MEDIA_ROOT = os.path.join(BASE_DIR, "media")
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
